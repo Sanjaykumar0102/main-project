d165cfece43858fe6634fbebfc68ac07c7d22cb5
Sanjaykumar0102 <sanjaykumarkulla5@gmail.com>
2026-01-12 15:20:42 +0530

    Fix: Add pendingTaskReminder to Inngest serve routes

--- server/routes/inngestRoutes.js
+++ server/routes/inngestRoutes.js
@@ -1,12 +1,13 @@
 const { serve } = require("inngest/express");
 const { inngest } = require("../inngest/client");
-const { helloWorld, taskReminder, dailySummary } = require("../inngest/functions");
+const { helloWorld, taskReminder, pendingTaskReminder, dailySummary } = require("../inngest/functions");
 
 const router = serve({
     client: inngest,
     functions: [
         helloWorld,
-        taskReminder, // Future functions
+        taskReminder,
+        pendingTaskReminder,
         dailySummary
     ],
 });
